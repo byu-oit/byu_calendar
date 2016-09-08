d53bf377795ce67815157633af3958b1073464bb
katrialesser <katrialynn18@gmail.com>
2016-09-08 15:14:33 -0600

    changing page template for nav

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -9,29 +9,23 @@
       <?php if ($top_bar_classes): ?>
       <div class="<?php print $top_bar_classes; ?>">
       <?php endif; ?>
-        <nav class="top-bar"<?php print $top_bar_options; ?>>
+        <div class="top-bar"<?php print $top_bar_options; ?>
+            <div class="BYU-logo"><img source="../sites/all/themes/byu_calendar/images/BYU_IM54d_white.png"</div>
           <ul class="title-area">
             <li class="name"><h1><?php print $linked_site_name; ?></h1></li>
             <li class="toggle-topbar menu-icon"><a href="#"><span><?php print $top_bar_menu_text; ?></span></a></li>
           </ul>
-          <section class="top-bar-section">
-            <?php if ($top_bar_main_menu) :?>
-              <?php print $top_bar_main_menu; ?>
-            <?php endif; ?>
-            <?php if ($top_bar_secondary_menu) :?>
-              <?php print $top_bar_secondary_menu; ?>
-            <?php endif; ?>
-          </section>
-          <section class="nav-bar-section">
-            <?php if ($top_bar_main_menu) :?>
-              <?php print $top_bar_main_menu; ?>
-            <?php endif; ?>
-            <?php if ($top_bar_secondary_menu) :?>
-              <?php print $top_bar_secondary_menu; ?>
-            <?php endif; ?>
-          </section>  
-            
-        </nav>
+            <nav>
+              <section class="top-bar-section">
+                <?php if ($top_bar_main_menu) :?>
+                  <?php print $top_bar_main_menu; ?>
+                <?php endif; ?>
+                <?php if ($top_bar_secondary_menu) :?>
+                  <?php print $top_bar_secondary_menu; ?>
+                <?php endif; ?>
+              </section>
+            </nav>
+          </div>
       <?php if ($top_bar_classes): ?>
       </div>
       <?php endif; ?>
