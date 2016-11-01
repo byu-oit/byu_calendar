dddb9cd00ac44247f2d1c9a01cb183b9c16beae9
katrialesser <katrialynn18@gmail.com>
2016-11-01 10:22:52 -0600

    removed new top-bar-inner div and tweaked mini month

--- templates/calendar-mini--large-screen-calendar--block-6.tpl.php
+++ templates/calendar-mini--large-screen-calendar--block-6.tpl.php
@@ -91,7 +91,7 @@ $weekEndTime = $weekStartTime + (6*24*3600);
 
             //calc each day's week id
 //            $dayNumber = date_format($dateOb, 'z'); // returns 0 - 365
-            $thatWeek = 2+ floor(($dateTime - $week2StartTime)/(7 * 24 * 3600));
+            $thatWeek = 2+ floor( (($dateTime - $week2StartTime)+3600)/(7 * 24 * 3600) );
 
               if($rowid == '1' and $day > 24) {
                   $class = ' last-month';
