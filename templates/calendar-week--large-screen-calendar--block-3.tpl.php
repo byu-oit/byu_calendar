c666b4e15864321a2dca7ec2fb73f21b1bad5735
katrialesser <katrialynn18@gmail.com>
2016-11-02 15:35:28 -0600

    updated week template to handle if date was missing

--- templates/calendar-week--large-screen-calendar--block-3.tpl.php
+++ templates/calendar-week--large-screen-calendar--block-3.tpl.php
@@ -71,8 +71,33 @@ foreach ($day_names as $key => $value) {
         $current = current_path();
         //$current = 'askldjasldfkjasldf/calendar/week/2016-W33';
         $currentDate = substr($current, -8);  // 2016-W33
-        //cho 'current date is ' . $currentDate;
-        // '2016-W32';
+
+        $weekCheck = substr($current, -3,1);
+        if($weekCheck == 'W') {
+            // is normal
+        } else if ($weekCheck == 'w') {
+            $currentDate = substr($current, -8,5) . 'W' . substr($currentDate,-2);
+        } else {
+            // get current date, calc week id
+            $today = date ("Y-m-d");
+            $dayNumber = date ("z");
+            $year = date("Y");
+            $timeNow = time();
+
+            $startWYear = '1 January ' . $year;
+            $yearStartTime = strtotime($startWYear);
+            $todaysTime = $yearStartTime + ($dayNumber * 24 * 3600);
+
+            $yearStartWeekday = date('w', $yearStartTime);
+            //echo $year . ' started on a ' . $yearStartWeekday;
+            $weekStartJanDay = 7 - $yearStartWeekday;
+            $week2StartTime = $yearStartTime + ($weekStartJanDay*24*3600); 
+            $week = 2+ floor(($todaysTime - $week2StartTime)/(7 * 24 * 3600));
+            $currentDate = $year . '-W' . $week;
+        }
+        
+        
+        
 //        $dateOb = date_create_from_format('Y-W', $currentDate);
         $year = substr($currentDate, 0, 4);
         $lastYear = $year -1;
