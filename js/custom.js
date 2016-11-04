430e9c1b43d47ba28a7323ed4b7d58ff0441337a
katrialesser <katrialynn18@gmail.com>
2016-11-04 15:48:26 -0600

    trying to get things back to normal

--- js/custom.js
+++ js/custom.js
@@ -87,10 +87,12 @@ jQuery( document ).ready(function( $ ) {
 //                 console.log('u can see topbar');
                 $('.left-sidebar-calendar-exposed-filters').removeClass('scrolled'); 
                 $('.block-views-large-screen-calendar-block-6').removeClass('scrolled'); 
+                 $('.block-views-large-screen-calendar-block-7').removeClass('scrolled'); 
 //                $('.left-sidebar-minimonth').removeClass('scrolled'));
                 $('.block-block-11').removeClass('scrolled'); 
             } else {
                 $('.block-views-large-screen-calendar-block-6').addClass('scrolled');
+                $('.block-views-large-screen-calendar-block-7').addClass('scrolled');
 //                $('.left-sidebar-minimonth').addClass('scrolled');
                 $('.left-sidebar-calendar-exposed-filters').addClass('scrolled');
                 
