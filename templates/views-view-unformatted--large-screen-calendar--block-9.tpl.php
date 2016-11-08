e84cfbe1717d42be4bc6b61c969cb64ddaf2e8c5
katrialesser <katrialynn18@gmail.com>
2016-11-08 15:37:47 -0700

    updating title format

--- templates/views-view-unformatted--large-screen-calendar--block-9.tpl.php
+++ templates/views-view-unformatted--large-screen-calendar--block-9.tpl.php
@@ -8,7 +8,28 @@
  */
 ?>
 <?php if (!empty($title)): ?>
-  <h3><?php print $title; ?></h3>
+
+<?php
+$current = current_path();
+$currentDate = substr($current, -10);
+
+if (preg_match("/^(19|20)\d\d[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])$/", $currentDate)) {
+	// is normal
+} else {
+    $currentDate = date("Y-m-d");
+}
+$year = substr($currentDate, 0, 4);
+$mid = substr($currentDate, -5, 2);
+$did = substr($currentDate, -2);
+
+
+
+$titleFormatted = date("j |  l, F Y", mktime(0, 0, 0, $mid, $did, $year));
+
+
+?>
+
+  <h3><?php print $titleFormatted; ?></h3>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
   <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
