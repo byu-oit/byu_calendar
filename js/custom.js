30ee640ae10145b2ded0de854755a44a88a57f80
katrialesser <katrialynn18@gmail.com>
2016-12-12 16:13:01 -0700

    if tickets exist move share block

--- js/custom.js
+++ js/custom.js
@@ -534,9 +534,12 @@ function startListeners() {
 	
 //});
     
-     if tickets button does not exist, move share button up
-	if($('span.add-to-calendar').hasClass('tickets-2') ){
-		$('section.block-social-share-social-share').css("margin", "-131px 0px 10px 435px");
+//     if tickets button does not exist, move share button up
+	if($(body).hasClass('node-type-event') && $(body).hasClass('large-screen')) {
+		if( $('.views-field-field-tickets-url').length )
+		{
+			$('section.block-social-share-social-share').css("margin", "-131px 0px 10px 435px");	
+		}	
 	}
 //    
 //	/* --- subscribe button ---- */
