7b597c4aeca5654fc69406e0fedaa01f6173fff7
katrialesser <katrialynn18@gmail.com>
2017-01-12 14:36:43 -0700

    trying to remove all day from output

--- templates/views-view-field--api--field-event-date-1.tpl.php
+++ templates/views-view-field--api--field-event-date-1.tpl.php
@@ -29,5 +29,7 @@
 
 // 2017011800Z (All day)
 //<span class="date-display-single" property="dc:date" datatype="xsd:dateTime" content="2017-01-17T16:00:00-07:00">20170117T160000Z</span>
-print 'a test date here';
+$output = preg_replace("/(All day)/", "", $output);
+
+print $output;
 ?>
