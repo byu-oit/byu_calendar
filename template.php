1bbf7b53b19bae3c19e4d217e36c66576a9f5ce6
katrialesser <katrialynn18@gmail.com>
2017-02-21 13:02:07 -0700

    changing cdn and hoeffler font file to use just // instead of https so its not hard coded

--- template.php
+++ template.php
@@ -10,8 +10,8 @@ function byu_calendar_preprocess_html(&$variables) {
 // drupal_add_css('http://ajax.googleapis.com/ajax/libs/angular_material/1.1.0-rc2/angular-material.min.css', array('weight' => CSS_THEME, 'browsers' => array('!IE' => FALSE), 'preprocess' => FALSE));
 //  // Need legacy support for IE downgrade to Foundation 2 or use JS file below
 //  // drupal_add_js('http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE7.js', 'external');
-    drupal_add_css('https://cloud.typography.com/75214/6517752/css/fonts.css', array('type' => 'external'));
-	drupal_add_js('https://cdn.byu.edu/2017-core-components/unstable/bootstrap.js', 'external');
+    drupal_add_css('//cloud.typography.com/75214/6517752/css/fonts.css', array('type' => 'external'));
+	drupal_add_js('//cdn.byu.edu/2017-core-components/unstable/bootstrap.js', 'external');
 	
 	
     // Populate the body classes.
