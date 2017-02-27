0ebd233df9519af46fedbcfd504eb81916275cab
katrialesser <katrialynn18@gmail.com>
2017-02-27 12:53:10 -0700

    removed my old header

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -42,107 +42,14 @@
     <byu-search slot="search">
 <!--		<div id="top-bar-search"> -->
 		  <?php 
-
+// they are working on allowing forms, when that works, enable this line again:
 //		  $search = drupal_get_form('search_block_form'); print render($search); 
 		  ?>
 <!--	 	</div>-->
 	</byu-search>
   </byu-header>
   <!--.l-header region -->
-  <header role="banner" class="l-header" <?php  /* print header options like if it has a menu or no menu */ ?>
-
-    <?php if ($top_bar): ?>
-      <!--.top-bar -->
-      <?php if ($top_bar_classes): ?>
-      <div class="<?php print $top_bar_classes; ?>">
-      <?php endif; ?>
-
-        <nav class="top-bar"<?php print $top_bar_options; ?>>
-          <div id="top-bar-inner">  
-              <ul class="title-area">
-                <li class="name"><h1><?php print $linked_site_name; ?></h1></li>
-                <li class="toggle-topbar menu-icon"><a href="#"><span><?php print $top_bar_menu_text; ?></span></a></li>
-              </ul>
-			  <div id="top-right-inner">
-				  <div id="top-bar-search"> 
-					  <?php 
-						//print $search_box; 
-//					  $block = module_invoke('search', 'block_view', 'search');
-//						print render($block); 
-					  $search = drupal_get_form('search_block_form'); print render($search); 
-					  ?>
-				  </div>
-
-				  <div id="sign-in"><a href="/user">Sign In <i class="fa fa-user" aria-hidden="true"></i></a></div>
-				  <div id="sign-out"><a href="/caslogout">Sign Out <i class="fa fa-user" aria-hidden="true"></i></a></div>
-			</div>
-              <section class="top-bar-section">
-                <?php if ($top_bar_main_menu) :?>
-                  <?php print $top_bar_main_menu; ?>
-                <?php endif; ?>
-                <?php if ($top_bar_secondary_menu) :?>
-                  <?php print $top_bar_secondary_menu;  ?>
-                <?php endif; ?>
-              </section>
-            </div>
-        </nav>
-      <?php if ($top_bar_classes): ?>
-      </div>
-      <?php endif; ?>
-      <!--/.top-bar -->
-    <?php endif; ?>
-
-    <!-- Title, slogan and menu -->
-    <?php if ($alt_header): ?>
-    <section class="row <?php print $alt_header_classes; ?>">
-
-      <?php if ($linked_logo): print $linked_logo; endif; ?>
-
-      <?php if ($site_name): ?>
-        <?php if ($title): ?>
-          <div id="site-name" class="element-invisible">
-            <strong>
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </strong>
-          </div>
-        <?php else: /* Use h1 when the content title is empty */ ?>
-          <h1 id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-          </h1>
-        <?php endif; ?>
-      <?php endif; ?>
-
-      <?php if ($site_slogan): ?>
-        <h2 title="<?php print $site_slogan; ?>" class="site-slogan"><?php print $site_slogan; ?></h2>
-      <?php endif; ?>
-
-      <?php if ($alt_main_menu): ?>
-        <nav id="main-menu" class="navigation" role="navigation">
-          <?php print ($alt_main_menu); ?>
-        </nav> <!-- /#main-menu -->
-      <?php endif; ?>
-
-      <?php if ($alt_secondary_menu): ?>
-        <nav id="secondary-menu" class="navigation" role="navigation">
-          <?php print $alt_secondary_menu; ?>
-        </nav> <!-- /#secondary-menu -->
-      <?php endif; ?>
-
-    </section>
-    <?php endif; ?>
-    <!-- End title, slogan and menu -->
-
-    <?php if (!empty($page['header'])): ?>
-      <!--.l-header-region -->
-      <section class="l-header-region row">
-        <div class="large-12 columns">
-          <?php print render($page['header']); ?>
-        </div>
-      </section>
-      <!--/.l-header-region -->
-    <?php endif; ?>
-
-  </header>
+  
   <!--/.l-header -->
 
   <?php if (!empty($page['featured'])): ?>
