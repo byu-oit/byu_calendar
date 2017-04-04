e686afbdae7f24322ca616806fe11bbe8a7b7d72
katrialesser <katrialynn18@gmail.com>
2017-04-04 12:36:17 -0600

    adding closing div to test how link printed outside of inner

--- templates/calendar-month-col.tpl.php
+++ templates/calendar-month-col.tpl.php
@@ -19,7 +19,7 @@ $headers = (isset($item['header_id'])) ? ' headers="'. $item['header_id'] .'" ' 
     $id = $item['id'];
     $idEnd = substr($id, -8);
     if ($idEnd !== 'date-box') {
-        echo '<div class="link-to-day"><a href="../day/' . $item['date'] . '">SEE MORE ></a></div>';
+        echo '</div><div class="link-to-day"><a href="../day/' . $item['date'] . '">SEE MORE ></a></div>';
     }
     ?>
 <!--    </div>-->
