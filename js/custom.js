e3dd8eb426667083438dca573a325de010cd2bd5
Katria Lesser <katrialynn18@gmail.com>
2017-10-09 15:51:40 -0600

    added 1 delay

--- js/custom.js
+++ js/custom.js
@@ -101,24 +101,17 @@ jQuery( document ).ready(function( $ ) {
 	// handle mobile left sidebar positioning
 
 	if ($("body").hasClass("one-sidebar") && $("body").hasClass("mobile-first")) {
-
-		function adjustForFooter() {
-			if ($('byu-header.byu-component-rendered').size() == 0) {
-				console.log('TEEEEEEST');
-				window.setTimeout(adjustForFooter, 100);
-				return;
-			}
-            var mainContainerHeight = document.getElementById('main-container').offsetHeight;
-            // var mainContainerHeight = document.getElementById('main-container').style.height;
-            console.log('main height is', mainContainerHeight);
-            var distToFooter = $('#byu-footer').offset().top;
-            var mobileHeaderHeight = 55 + 35;
-            document.getElementById('page-container').style.height = (mainContainerHeight) + 'px';
-            // document.getElementById('page-container').style.height = (distToFooter - mobileHeaderHeight) + 'px';
-            console.log('disttofooter ', distToFooter);
-            console.log('mobileHEaderHeight ', mobileHeaderHeight);
-        };
-		adjustForFooter();
+		window.setTimeout(function() {
+			var mainContainerHeight = document.getElementById('main-container').offsetHeight;
+			// var mainContainerHeight = document.getElementById('main-container').style.height;
+			console.log('main height is', mainContainerHeight);
+			var distToFooter = $('#byu-footer').offset().top;
+			var mobileHeaderHeight = 55 + 35;
+			document.getElementById('page-container').style.height = (mainContainerHeight) + 'px';
+			// document.getElementById('page-container').style.height = (distToFooter - mobileHeaderHeight) + 'px';
+			console.log('disttofooter ', distToFooter);
+			console.log('mobileHEaderHeight ', mobileHeaderHeight);
+        }, 1);
 	}
 
 
